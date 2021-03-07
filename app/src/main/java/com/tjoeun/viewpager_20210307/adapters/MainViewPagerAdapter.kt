5572c9e01130d6076a4eb1f74f951ddd37d838ce
seoyeongkang <jumprope123@naver.com>
2021-03-07 12:48:08 +0900

    뷰페이저 어댑터 클래스 추가

---
+++ app/src/main/java/com/tjoeun/viewpager_20210307/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.tjoeun.viewpager_20210307.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.tjoeun.viewpager_20210307.fragments.BirthFragment
+import com.tjoeun.viewpager_20210307.fragments.DescFragment
+import com.tjoeun.viewpager_20210307.fragments.NickFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        if(position == 0) {
+            return NickFragment()
+        }
+        else if(position == 1){
+            return BirthFragment()
+        }
+        else {
+            return DescFragment()
+        }
+    }
+
+}
